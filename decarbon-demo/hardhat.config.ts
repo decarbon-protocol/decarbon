af86975704b6efd5d9cf3c17bfc658651fa08e8b
duckmint864 <nddminh2021@gmail.com>
2023-08-14 19:07:13 +0700

    Update config

--- decarbon-demo/hardhat.config.ts
+++ decarbon-demo/hardhat.config.ts
@@ -6,7 +6,7 @@ import "solidity-coverage";
 import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/types";
 
-const PRIVATE_KEYS: string[] = process.env.PRIVATE_KEY?.split(" ") ?? [""];
+const PRIVATE_KEYS: string[] = process.env.PRIVATE_KEYS?.split(" ") ?? [""];
 const SEPOLIA_RPC_URL: string = process.env.SEPOLIA_RPC_URL ?? "";
 const MAINNET_RPC_URL: string = process.env.MAINNET_RPC_URL ?? "";
 const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY ?? "";
