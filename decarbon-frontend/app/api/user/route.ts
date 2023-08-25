46ccacdaa7c722f8e8d7dceacce1b16010ac7e49
phuctanpham <phuctanpham@gmail.com>
2023-08-25 11:10:09 +0000

    blocking api/user

--- decarbon-frontend/app/api/user/route.ts
+++ decarbon-frontend/app/api/user/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 import prisma from '@/lib/prisma';
 
-export async function GET() {
-  const result = await prisma.blocks.findMany();
-  return NextResponse.json(result);
-}
+//export async function GET() {
+//  const result = await prisma.blocks.findMany();
+//  return NextResponse.json(result);
+//}
