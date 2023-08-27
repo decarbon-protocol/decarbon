df013fcb459f7b9a78dddb77770c306efd8c198a
duckmint864 <nddminh2021@gmail.com>
2023-08-27 21:45:07 +0700

    Fix error HTTP status code 400 (bad request)

--- decarbon-demo/scripts/aggregate/use_json_rpc/get_tx_from_hashes.ts
+++ decarbon-demo/scripts/aggregate/use_json_rpc/get_tx_from_hashes.ts
@@ -1,50 +1,55 @@
 import axios, { AxiosError } from 'axios';
-import { url } from "./";
+import { get_tx_of_block, url } from "./";
 import { output } from '../../utils';
+import { exampleBlock } from '../../interfaces';
 
 export default async function get_tx_from_hashes(transactionHashes: string[])
-: Promise<[Record<string, string | boolean | []>[], Record<string, string | boolean | []>[]] | undefined>{
-    const batchRequests = transactionHashes.flatMap((hash, index) => [
-        {
-            jsonrpc: '2.0',
-            method: 'eth_getTransactionReceipt',
-            params: [hash],
-            id: index * 2 + 1
-        },
-        {
-            jsonrpc: '2.0',
-            method: 'eth_getTransactionByHash',
-            params: [hash],
-            id: index * 2 + 2
-        }
-    ]);
-    const MAX_RETRY_ATTEMPTS = 5;
-    let retryCount = 0;
-    while (retryCount < MAX_RETRY_ATTEMPTS) {
-        try {
-            const response = await axios.post(url, batchRequests);
-            const receiptResults: Record<string, string | boolean | []>[] = response.data.filter((result: any, index: number) => index % 2 === 0).map((result: any) => result.result);
-            const transactionResults: Record<string, string | boolean | []>[] = response.data.filter((result: any, index: number) => index % 2 === 1).map((result: any) => result.result);
-            return [receiptResults, transactionResults];
-        } catch (err) {
-            if (axios.isAxiosError(err)) {
-                // console.error(`Server error on attempt ${retryCount + 1}:`, err);
-                output(`Server error on attempt ${retryCount + 1}, HTTP status code: ${err.response?.status}`);
-                await new Promise<void>(resolve => {
-                    setTimeout(resolve, 12 * 1000);
-                });
-                retryCount++;
-                // console.log("Retrying...");
-                output("Retrying...");
+    : Promise<[Record<string, string | boolean | []>[], Record<string, string | boolean | []>[]] | undefined> {
+    const MAX_HASHES_PER_REQUEST = 166;
+    while (transactionHashes.length > 0) {
+        let requestList = transactionHashes.splice(0, MAX_HASHES_PER_REQUEST - 1);
+        const batchRequests = requestList.flatMap((hash, index) => [
+            {
+                jsonrpc: '2.0',
+                method: 'eth_getTransactionReceipt',
+                params: [hash],
+                id: index * 2 + 1
+            },
+            {
+                jsonrpc: '2.0',
+                method: 'eth_getTransactionByHash',
+                params: [hash],
+                id: index * 2 + 2
             }
-            else {
-                throw new Error(`get_tx_from_hashes(): ${err}`);
+        ]);
+        const MAX_RETRY_ATTEMPTS = 5;
+        let retryCount = 0;
+        while (retryCount < MAX_RETRY_ATTEMPTS) {
+            try {
+                const response = await axios.post(url, batchRequests);
+                const receiptResults: Record<string, string | boolean | []>[] = response.data.filter((result: any, index: number) => index % 2 === 0).map((result: any) => result.result);
+                const transactionResults: Record<string, string | boolean | []>[] = response.data.filter((result: any, index: number) => index % 2 === 1).map((result: any) => result.result);
+                return [receiptResults, transactionResults];
+            } catch (err) {
+                if (axios.isAxiosError(err)) {
+                    // console.error(`Server error on attempt ${retryCount + 1}:`, err);
+                    output(`Server error on attempt ${retryCount + 1}, HTTP status code: ${err.response?.status}`);
+                    await new Promise<void>(resolve => {
+                        setTimeout(resolve, 12 * 1000);
+                    });
+                    retryCount++;
+                    // console.log("Retrying...");
+                    output("Retrying...");
+                }
+                else {
+                    throw new Error(`get_tx_from_hashes(): ${err}`);
+                }
             }
         }
+        // If maximum attempts exceeded, print out error and don't return anything
+        // console.error("\tMaximum retry attempts exceeded.");
+        output("\tMaximum retry attempts exceeded.");
     }
-    // If maximum attempts exceeded, print out error and don't return anything
-    // console.error("\tMaximum retry attempts exceeded.");
-    output("\tMaximum retry attempts exceeded.");
 }
 
 // Testing
@@ -61,12 +66,16 @@ export default async function get_tx_from_hashes(transactionHashes: string[])
 //     "0xafba6e09095b04a8392d4d3786853436e4affb0c9dab9df608d7e410177c6968",
 //     "0x1a30677da5f8d36fa4176dd3a3a50a5f3dc23549b6642402a3bd2cf5ddc20d92",
 // ]
-// get_tx_from_tx_hashes(mocks)
-//     .then(([receipts, transactions]) => {
-//         console.log('Transaction Receipts:', receipts);
-//         console.log('Transaction Details:', transactions);
+
+
+// get_tx_of_block(exampleBlock, false).then(() => {
+//     get_tx_from_hashes(exampleBlock.tx_hashes!)
+//     .then(() => {
+//         // console.log('Transaction Receipts:', receipts);
+//         // console.log('Transaction Details:', transactions);
 //         // Process the data as needed
 //     })
-//     .catch(error => {
+//     .catch((error: any) => {
 //         console.error('Error:', error);
-//     });
+//     });  
+// })
